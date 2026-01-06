5409c1f4aa46d50e278b1f4e24589e58051ca368
gkfat <gkgkdesign@gmail.com>
2026-01-06 14:09:34 +0800

    fix: 精選專案增加到 6 個

--- views/home/FeaturesView.vue
+++ views/home/FeaturesView.vue
@@ -104,6 +104,8 @@ const featuredWorkSlugs = [
     '2024-welcabin-liff', 
     '2021-tsmc',
     '2023-logicard-duel',
+    '2021-ikea',
+    '2025-punch-qrcode-generator',
 ];
 
 const { data: allWorks } = await useAsyncData('featured-works', () => 
