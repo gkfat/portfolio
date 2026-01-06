c7c06c491c4336b3a53af02ff3880c8b0ed7c222
gkfat <gkgkdesign@gmail.com>
2026-01-06 12:30:15 +0800

    fix: route 出現 error 時轉導回 root path

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div />
+</template>
+
+<script setup lang="ts">
+const error = useError();
+
+// 其他錯誤也重導向到 / (可選)
+if (error.value) {
+    await navigateTo('/', { replace: true });
+}
+</script>
