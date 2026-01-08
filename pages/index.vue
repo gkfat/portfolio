13b1ba1d005a57a75a6ec0e00931f844214b4b4f
gkfat <gkgkdesign@gmail.com>
2026-01-08 21:35:01 +0800

    fix: useSeo in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <script setup lang="ts">
-definePageMeta({ redirect: '/home' });
+useSeo({ key: 'home' });
+
+onMounted(() => {
+    setTimeout(() => {
+        navigateTo('/home');
+    }, 500);
+});
 </script>
+
+<template>
+    <div class="position-fixed top-0 left-0 right-0 bottom-0 bg-background d-flex justify-center align-center" :style="{zIndex: 3000}">
+        <h1 class="text-h2 text-secondary font-weight-bold">Hello!</h1>
+    </div>
+</template>
